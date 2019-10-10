467518f10db1d773dcea29ff9458e15d900c7c0f
vincentlee1218 <44049108+vincentlee1218@users.noreply.github.com>
2019-10-10 15:08:19 -0700

    Version 2.0
     Cover up to Slide 25 in presentation.
    
    https://docs.google.com/presentation/d/1DxO00nFj93fWWFI1W9DmpEDLBW6EFnlDlfwRlHCXXJI/edit

--- AquaSimApplication.java
+++ AquaSimApplication.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.util.Random;
+import java.util.Scanner;
 
 /** Aquarium Lab Series: <br>    
  *      The AquaSimApplication class contains the main function that will
@@ -21,12 +22,21 @@ public class AquaSimApplication
      *  This is the main function.  It executes the program.
      *  @param    String args[] is never used
      **/
+    private static Random generator = new Random();
     public static void main(String args[])
     {
         System.out.println("Welcome to the AquaSim application.");
 
         // CONSTRUCT OBJECTS NEEDED FOR THE AQUARIUM SIMULATION.
-        Random generator = new Random();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Do you want to set the steps? y/n");
+        String nSteps = s.nextLine();
+        boolean promptSteps=false;
+        if (nSteps.equals("y")) promptSteps = true;
+        
+        int randNum;
+        Color colorFish = Color.WHITE;
+        
         // int randNum = generator.nextInt(10);
 
         // Construct the aquarium.  Specify its dimensions when creating it.
@@ -34,15 +44,21 @@ public class AquaSimApplication
         //aqua = new Aquarium(600, 480); // ... object that has now been created
         aqua = new Aquarium(600, 480);
         // Construct fish and add them to the aquarium.
-        AquaFish fish1 = new AquaFish(aqua, Color.RED);aqua.add(fish1);
-        AquaFish fish2 = new AquaFish(aqua, Color.BLUE);aqua.add(fish2);
-        AquaFish nemo = new AquaFish(aqua, Color.BLUE);aqua.add(nemo);
         
-        // Construct a graphical user interface (GUI) to display and control
+        colorFish = randomColor();
+        AquaFish fish1 = new AquaFish(aqua, colorFish);aqua.add(fish1);
+        colorFish = randomColor();
+        AquaFish fish2 = new AquaFish(aqua, colorFish);aqua.add(fish2);
+        colorFish = randomColor();
+        AquaFish nemo = new AquaFish(aqua, colorFish);aqua.add(nemo);
+        
+        // Construct a graphkical user interface (GUI) to display and control
         // the simulation.  The user interface needs to know about the
         // aquarium, so we pass aqua to the user interface constructor.
         AquaSimGUI userInterface;              // create reference to GUI ...
-        userInterface = new AquaSimGUI(aqua);  // ... and then GUI itself
+        userInterface = new AquaSimGUI(aqua, promptSteps);
+        
+        // ... and then GUI itself
 
         // Tell the user how to start the aquarium simulation.
         System.out.println("Press the Start button to start the simulation.");
@@ -59,16 +75,44 @@ public class AquaSimApplication
         // Remind user how to quit application.
         userInterface.println ("Close GUI display window to quit.");
         // Make the fish move and redisplay
-        for(int i = 0; i<30; i--) {
+        int steps = userInterface.getNumberOfSteps();
+        System.out.println(steps);
+        for(int i = 0; i<steps; i++) {
         
-        if (fish1.atWall()) fish1.changeDir();fish1.moveForward();
-        if (fish2.atWall()) fish2.changeDir();fish2.moveForward();
-        if (nemo.atWall()) nemo.changeDir();nemo.moveForward();
+        if (fish1.atWall() || randomDirDecis()==0) fish1.changeDir();fish1.moveForward();
+        if (fish2.atWall() || randomDirDecis()==0) fish2.changeDir();fish2.moveForward();
+        if (nemo.atWall() || randomDirDecis()==0) nemo.changeDir();nemo.moveForward();
         
         userInterface.showAquarium();
-    }
+        }
+        userInterface.println("Aquarium run for " + steps + " steps."); 
 
         // WRAP UP.
 
     }//end main
+    
+    public static Color randomColor() {
+        int randNum = generator.nextInt(1000);
+        if (randNum < 140) return Color.RED;
+        if (randNum < 280) return Color.ORANGE;
+        if (randNum < 420) return Color.YELLOW;
+        if (randNum < 560) return Color.GREEN;
+        if (randNum < 800) return Color.BLUE;
+        if (randNum < 900) return Color.MAGENTA;
+        return Color.WHITE;
+        
+        /*
+        switch (randNum) {
+            case 0: return Color.RED;
+            case 1: return Color.ORANGE;
+            case 2: return Color.YELLOW;
+            case 3: return Color.GREEN;
+            case 4: return Color.BLUE;
+            case 5: return Color.MAGENTA;
+            default: return Color.WHITE;
+        }
+        */
+    }
+    
+    public static int randomDirDecis() {return generator.nextInt(4);}
 }//end class
